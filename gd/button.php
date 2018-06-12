0bf5270278330180536cd5251b324cdcee1d7eb5
Ilya Goss <gossoudarev@herzen.spb.ru>
2018-06-12 18:32:22 +0300

    Update button.php

--- gd/button.php
+++ gd/button.php
@@ -1,8 +1,8 @@
-<?php ## Создание картинки "на лету".
+<?php ## Создание картинки «на лету».
   // Получаем строку, которую нам передали в параметрах
-  $string = $_SERVER['QUERY_STRING'] ?? "Hello, world!";
+  $string = $_SERVER['QUERY_STRING'] ?? 'Hello, world!';
   // Загружаем рисунок фона с диска.
-  $im = imageCreateFromGif("button.gif");
+  $im = imageCreateFromGif('button.gif');
   // Создаем в палитре новый цвет - черный.
   $color = imageColorAllocate($im, 0, 0, 0);
   // Вычисляем размеры текста, который будет выведен.
@@ -10,10 +10,9 @@
   // Выводим строку поверх того, что было в загруженном изображении.
   imageString($im, 3, $px, 1, $string, $color);
   // Сообщаем о том, что далее следует рисунок PNG.
-  header("Content-type: image/png");
+  header('Content-type: image/png');
   // Теперь - самое главное: отправляем данные картинки в
   // стандартный выходной поток, т. е. в браузер.
   imagePng($im);
   // В конце освобождаем память, занятую картинкой.
   imageDestroy($im);
-?>
