43db77f78cadf7256fbaa7e25c4b806cc125a438
Ilya Goss <gossoudarev@herzen.spb.ru>
2018-06-12 19:07:48 +0300

    Update ttf.php

--- gd/ttf.php
+++ gd/ttf.php
@@ -1,18 +1,18 @@
 <?php ## Пример работы с TTF-шрифтом.
-  require_once "lib/imagettf.php";
-  // Выводимая строка. 
-  $string = "Привет, мир!";
+  require_once 'lib/imagettf.php';
+  // Выводимая строка.
+  $string = 'Привет, мир!';
   // Шрифт.
-  $font = getcwd()."/times.ttf";
+  $font = getcwd().'/OpenSans.ttf'; // http://kodaktor.ru/OpenSans.ttf
   // Загружаем фоновой рисунок.
-  $im = imageCreateFromPng("sample02.png");
+  $im = imageCreateFromPng('sample02.png'); // http://kodaktor.ru/sample02.png
   // Угол поворота зависит от текущего времени.
   $angle = (microtime(true) * 10) % 360;
   // Ксли хотите, чтобы текст шел из угла в угол, раскомментируйте строчку:
   # $angle = rad2deg(atan2(imageSY($im), imageSX($im)));
   // Подгоняем размер текста под размер изображения.
   $size = imageTtfGetMaxSize(
-    $angle, $font, $string, 
+    $angle, $font, $string,
     imageSX($im), imageSY($im)
   );
   // Создаем в палитре новые цвета
@@ -22,12 +22,11 @@
   $sz = imageTtfSize($size, $angle, $font, $string);
   $x = (imageSX($im) - $sz[0]) / 2 + $sz[2];
   $y = (imageSY($im) - $sz[1]) / 2 + $sz[3];
-  // Рисуем строку текста, вначале черным со сдвигом, а затем - 
+  // Рисуем строку текста, вначале черным со сдвигом, а затем -
   // основным цветом поверх (чтобы создать эффект тени).
   imageTtfText($im, $size, $angle, $x + 3, $y + 2, $shadow, $font, $string);
   imageTtfText($im, $size, $angle, $x, $y, $color, $font, $string);
   // Сообщаем о том, что далее следует рисунок PNG.
-  Header("Content-type: image/png");
+  Header('Content-type: image/png');
   // Выводим рисунок
   imagePng($im);
-?>
